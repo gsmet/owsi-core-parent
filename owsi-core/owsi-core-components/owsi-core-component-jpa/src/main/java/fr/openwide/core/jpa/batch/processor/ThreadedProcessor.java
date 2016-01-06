1c80e07df6b856eeec8d3079b9b088c79251b2b6
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-01-06 18:01:42 +0100

    Added error handling to ThreadedProcessor and MultithreadedBatchExecutor

--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/batch/processor/ThreadedProcessor.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/batch/processor/ThreadedProcessor.java
@@ -2,12 +2,19 @@ package fr.openwide.core.jpa.batch.processor;
 
 import static fr.openwide.core.jpa.more.property.JpaMorePropertyIds.MIGRATION_LOGGING_MEMORY;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -16,7 +23,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 
 import fr.openwide.core.jpa.batch.monitor.ProcessorMonitorContext;
 import fr.openwide.core.jpa.batch.monitor.ThreadLocalInitializingCallable;
@@ -28,43 +42,49 @@ public class ThreadedProcessor {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ThreadedProcessor.class);
 
+	private static final RejectedExecutionHandler THROW_EXCEPTION_ON_REJECTED_EXECUTION = new ThreadPoolExecutor.AbortPolicy();
+
+	private static final Function<Runnable, Callable<Object>> RUNNABLE_TO_CALLABLE = new Function<Runnable, Callable<Object>>() {
+		@Override
+		public java.util.concurrent.Callable<Object> apply(Runnable input) {
+			return input == null ? null : Executors.callable(input);
+		}
+	};
+
 	private final int threadPoolSize;
 	private final int keepAliveTime;
 	private final TimeUnit keepAliveTimeUnit;
 	private final int maxTotalDuration;
 	private final TimeUnit maxTotalDurationTimeUnit;
+
+	private final boolean abortAllOnError;
+	
 	private final Integer loggingCheckIntervalTime;
 	private final TimeUnit loggingCheckIntervalTimeUnit;
 	private final Integer maxLoggingTime;
 	private final TimeUnit maxLoggingTimeUnit;
 	private final Integer maxLoggingIncrement;
 	private final Logger progressLogger;
-	private final IPropertyService propertyService;
 	
-	private String loggerContext;
+	private final IPropertyService propertyService;
 
 	private ProcessorMonitorContext monitorContext;
 
-	public ThreadedProcessor(int threadPoolSize,
-			int maxTotalDuration, TimeUnit maxTotalDurationUnit,
-			int keepAliveTime, TimeUnit keepAliveTimeUnit,
-			IPropertyService propertyService) {
-		this(threadPoolSize, maxTotalDuration, maxTotalDurationUnit, keepAliveTime, keepAliveTimeUnit, propertyService,
-				null, null, null, null, null, null);
+	public ThreadedProcessor(int threadPoolSize, int maxTotalDuration, TimeUnit maxTotalDurationUnit, int keepAliveTime,
+			TimeUnit keepAliveTimeUnit, boolean abortAllOnError, IPropertyService propertyService) {
+		this(threadPoolSize, maxTotalDuration, maxTotalDurationUnit, keepAliveTime, keepAliveTimeUnit, abortAllOnError,
+				propertyService, null, null, null, null, null, null);
 	}
 
-	public ThreadedProcessor(int threadPoolSize,
-			int maxTotalDuration, TimeUnit maxTotalDurationUnit,
-			int keepAliveTime, TimeUnit keepAliveTimeUnit,
-			IPropertyService propertyService,
-			Integer loggingCheckIntervalTime, TimeUnit loggingCheckIntervalTimeUnit,
-			Integer maxLoggingTime, TimeUnit maxLoggingTimeUnit,
-			Integer maxLoggingIncrement,
-			Logger progressLogger) {
+	public ThreadedProcessor(int threadPoolSize, int maxTotalDuration, TimeUnit maxTotalDurationUnit, int keepAliveTime,
+			TimeUnit keepAliveTimeUnit, boolean abortAllOnError, IPropertyService propertyService,
+			Integer loggingCheckIntervalTime, TimeUnit loggingCheckIntervalTimeUnit, Integer maxLoggingTime,
+			TimeUnit maxLoggingTimeUnit, Integer maxLoggingIncrement, Logger progressLogger) {
 		super();
 		this.threadPoolSize = threadPoolSize;
 		this.keepAliveTime = keepAliveTime;
 		this.keepAliveTimeUnit = keepAliveTimeUnit;
+		this.abortAllOnError = abortAllOnError;
 		this.maxTotalDuration = maxTotalDuration;
 		this.maxTotalDurationTimeUnit = maxTotalDurationUnit;
 		this.loggingCheckIntervalTime = loggingCheckIntervalTime;
@@ -76,74 +96,124 @@ public class ThreadedProcessor {
 		this.propertyService = propertyService;
 	}
 
-	public <T> List<Future<T>> runWithoutTransaction(String loggerContext, List<Callable<T>> callables) {
-		return runWithoutTransaction(loggerContext, callables, null);
+	public <T> void runWithoutTransaction(String loggerContext, Collection<? extends Runnable> runnables)
+			throws ExecutionException {
+		runWithoutTransaction(loggerContext, runnables, null);
 	}
 
-	public <T> List<Future<T>> runWithoutTransaction(String loggerContext, List<Callable<T>> callables, Integer totalItems) {
-		return runWithTransaction(loggerContext, callables, null, totalItems);
+	public <T> void runWithoutTransaction(String loggerContext, Collection<? extends Runnable> runnables,
+			Integer totalItems) throws ExecutionException {
+		runWithTransaction(loggerContext, runnables, null, totalItems);
 	}
 
-	public <T> List<Future<T>> runWithTransaction(String loggerContext, List<Callable<T>> callables, TransactionTemplate transactionTemplate, Integer totalItems) {
-		this.loggerContext = loggerContext;
+	public <T> void runWithTransaction(final String loggerContext, Collection<? extends Runnable> runnables,
+			TransactionTemplate transactionTemplate, Integer totalItems) throws ExecutionException {
+		callWithTransaction(loggerContext, Collections2.transform(runnables, RUNNABLE_TO_CALLABLE),
+				transactionTemplate, totalItems);
+	}
+
+	public <T> List<T> callWithoutTransaction(String loggerContext, Collection<? extends Callable<T>> callables)
+			throws ExecutionException {
+		return callWithoutTransaction(loggerContext, callables, null);
+	}
+
+	public <T> List<T> callWithoutTransaction(String loggerContext, Collection<? extends Callable<T>> callables,
+			Integer totalItems) throws ExecutionException {
+		return callWithTransaction(loggerContext, callables, null, totalItems);
+	}
+
+	public <T> List<T> callWithTransaction(final String loggerContext, Collection<? extends Callable<T>> callables,
+			TransactionTemplate transactionTemplate, Integer totalItems) throws ExecutionException {
+		List<ListenableFuture<T>> futures = Lists.newArrayList();
 		
-		List<Future<T>> futures = Lists.newArrayList();
-		BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>();
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(threadPoolSize, threadPoolSize, keepAliveTime, keepAliveTimeUnit, workQueue);
-		executor.prestartAllCoreThreads();
+		// ThreadedPoolExecutor cannot be reused after shutdown, so we must instantiate it on each call.
+		BlockingQueue<Runnable> workingQueue = new LinkedBlockingQueue<Runnable>();
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+				threadPoolSize, threadPoolSize, keepAliveTime, keepAliveTimeUnit,
+				workingQueue, THROW_EXCEPTION_ON_REJECTED_EXECUTION);
+		ListeningExecutorService executor = MoreExecutors.listeningDecorator(threadPoolExecutor);
+		
+		threadPoolExecutor.prestartAllCoreThreads();
 		
 		Thread loggingThread = null;
 		this.monitorContext = new ProcessorMonitorContext();
 		if (progressLogger != null) {
-			loggingThread = new Thread(new LoggingRunnable());
+			loggingThread = new Thread(new LoggingRunnable(loggerContext));
 		}
 		if (totalItems != null) {
 			this.monitorContext.getTotalItems().set(totalItems);
 		}
 		try {
-			// all executions are wrapped in a separate transaction (if the template was provided)
-			for (Callable<T> callable : callables) {
-				Callable<T> wrappedCallable = new ThreadLocalInitializingCallable<>(callable, ProcessorMonitorContext.getThreadLocal(), monitorContext);
-				if (transactionTemplate != null) {
-					futures.add(executor.submit(new TransactionWrapperCallable<T>(transactionTemplate, wrappedCallable)));
-				} else {
-					futures.add(executor.submit(wrappedCallable));
-				}
-			}
-			
-			executor.shutdown();
 			if (loggingThread != null) {
 				loggingThread.start();
 			}
-			
-			List<Future<T>> terminatedFutures = Lists.newArrayList();
+
+			try {
+				List<FutureCallback<? super T>> callbacks = Lists.newArrayList();
+				if (abortAllOnError) {
+					callbacks.add(new ThreadPoolExecutorAbortFutureCallback(threadPoolExecutor, loggerContext));
+				}
+				for (Callable<T> callable : callables) {
+					Callable<T> wrappedCallable = new ThreadLocalInitializingCallable<>(callable,
+							ProcessorMonitorContext.getThreadLocal(), monitorContext);
+					if (transactionTemplate != null) {
+						// All executions are wrapped in a separate transaction
+						// (if the template was provided)
+						wrappedCallable = new TransactionWrapperCallable<>(transactionTemplate, wrappedCallable);
+					}
+
+					/*
+					 * The following call is blocking when all thread are
+					 * already executing a task, due to the use of
+					 * SynchronousQueue in the ThreadPoolExecutor. This means
+					 * that if the executor may be shut down before we finished
+					 * submitting all the tasks, which would trigger a
+					 * RejectedExecutionException.
+					 */
+					ListenableFuture<T> future = executor.submit(wrappedCallable);
+					
+					for (FutureCallback<? super T> callback : callbacks) {
+						Futures.addCallback(future, callback);
+					}
+					
+					futures.add(future);
+				}
+			} catch (RejectedExecutionException e) {
+				LOGGER.error(
+						"{} - Some tasks could not be submitted to the ThreadPoolExecutor."
+								+ " Submitted tasks: {} of {}."
+								+ " This is probably due to an error in another task causing the ThreadPoolExecutor to shut down.",
+						loggerContext, futures.size(), callables.size());
+			}
+
+			executor.shutdown();
+
+			List<T> results = Lists.newArrayList();
+			List<ExecutionException> executionExceptions = Lists.newArrayList();
 			boolean interrupted = false;
-			
+
 			try {
 				boolean terminated = executor.awaitTermination(maxTotalDuration, maxTotalDurationTimeUnit);
 				if (!terminated) {
-					LOGGER.error("Tasks haven't terminated before the timeout of {} {}", maxTotalDuration, maxTotalDurationTimeUnit.name());
+					LOGGER.error("{} - Tasks haven't terminated before the timeout of {} {}", maxTotalDuration,
+							maxTotalDurationTimeUnit.name());
 				}
 				LOGGER.info("{} - {} elements treated", loggerContext, this.monitorContext.getDoneItems());
 				if (this.monitorContext.getFailedItems().get() > 0) {
-					LOGGER.error("{} - {} elements in error", loggerContext, this.monitorContext.getFailedItems().get());
+					LOGGER.error("{} - {} elements in error", loggerContext,
+							this.monitorContext.getFailedItems().get());
 				}
 				if (this.monitorContext.getIgnoredItems().get() > 0) {
 					LOGGER.info("{} - {} elements ignored", loggerContext, this.monitorContext.getIgnoredItems().get());
 				}
-				
+
 				for (Future<T> future : futures) {
 					try {
-						future.get(0, TimeUnit.SECONDS);
-						if (future.isDone()) {
-							terminatedFutures.add(future);
-						} else {
-							interrupted = true;
-						}
+						T result = future.get(0, TimeUnit.SECONDS);
+						results.add(result);
 					} catch (ExecutionException e) {
-						// May never happen, since every callable was wrapped so that it catches exceptions
-						throw new IllegalStateException("Une tâche d'import d'objets a échoué ; interruption de l'import.", e);
-					} catch (TimeoutException timeoutException) {
+						executionExceptions.add(e);
+					} catch (CancellationException|TimeoutException timeoutException) {
 						// already notified by a warn
 						future.cancel(true);
 						interrupted = true;
@@ -152,12 +222,21 @@ public class ThreadedProcessor {
 			} catch (InterruptedException e) {
 				throw new IllegalStateException("Batch error: timeout.", e);
 			}
-			
+
+			if (!executionExceptions.isEmpty()) {
+				Iterator<ExecutionException> it = executionExceptions.iterator();
+				ExecutionException e = new ExecutionException(it.next().getCause());
+				while (it.hasNext()) {
+					e.addSuppressed(it.next().getCause());
+				}
+				throw e;
+			}
+
 			if (interrupted) {
 				throw new IllegalStateException("Batch error: a work thread was interrupted.");
 			}
-			
-			return terminatedFutures;
+
+			return results;
 		} finally {
 			ProcessorMonitorContext.unset();
 			if (loggingThread != null) {
@@ -165,7 +244,8 @@ public class ThreadedProcessor {
 				try {
 					loggingThread.join();
 				} catch (InterruptedException e) {
-					LOGGER.warn("Thread interrupted while waiting for the end of the logging thread execution.");
+					LOGGER.warn("{} - Thread interrupted while waiting for the end of the logging thread execution.",
+							loggerContext);
 				}
 			}
 		}
@@ -173,12 +253,19 @@ public class ThreadedProcessor {
 
 	protected class LoggingRunnable implements Runnable {
 		
+		private final String loggerContext;
+
 		private long startTime;
-		
+
 		private long lastLoggingTime;
-		
+
 		private int lastDoneItems;
-		
+
+		public LoggingRunnable(String loggerContext) {
+			super();
+			this.loggerContext = loggerContext;
+		}
+
 		@Override
 		public void run() {
 			startTime = System.currentTimeMillis();
@@ -196,30 +283,29 @@ public class ThreadedProcessor {
 			}
 			log(true);
 		}
-		
+
 		public void log(boolean force) {
 			long currentTime = System.currentTimeMillis();
 			int totalItems = monitorContext.getTotalItems().get();
 			int doneItems = monitorContext.getDoneItems().get();
 			int ignoredItems = monitorContext.getIgnoredItems().get();
-			if (force
-					|| (currentTime - lastLoggingTime) > maxLoggingTimeUnit.toMillis(maxLoggingTime)
+			if (force || (currentTime - lastLoggingTime) > maxLoggingTimeUnit.toMillis(maxLoggingTime)
 					|| (doneItems - lastDoneItems) > maxLoggingIncrement) {
 				Float speedSinceStart = (float) doneItems / (float) (currentTime - startTime);
 				int roundedSpeedSinceStart = Math.round(speedSinceStart * 1000);
-				
+
 				Float speedSinceLast = (float) (doneItems - lastDoneItems) / (float) (currentTime - lastLoggingTime);
 				int roundedSpeedSinceLast = Math.round(speedSinceLast * 1000);
-				
+
 				lastLoggingTime = currentTime;
 				lastDoneItems = doneItems;
-				
+
 				StringBuilder sb = new StringBuilder();
 				if (StringUtils.hasText(loggerContext)) {
 					sb.append(loggerContext).append(" - ");
 				}
 				sb.append("In progress: {} / {} ({} ignored, {} items/s since start, {} items/s since last log)");
-				
+
 				if (propertyService.get(MIGRATION_LOGGING_MEMORY)) {
 					sb.append(" - Available memory: {} / {}");
 					progressLogger.info(sb.toString(), doneItems, totalItems - ignoredItems, ignoredItems,
@@ -232,6 +318,54 @@ public class ThreadedProcessor {
 				}
 			}
 		}
+	}
+
+	private static class ThreadPoolExecutorAbortFutureCallback implements FutureCallback<Object> {
+		
+		private final ThreadPoolExecutor executor;
+
+		private final String loggerContext;
+		
+		public ThreadPoolExecutorAbortFutureCallback(ThreadPoolExecutor executor, String loggerContext) {
+			super();
+			this.executor = executor;
+			this.loggerContext = loggerContext;
+		}
+
+		@Override
+		public void onSuccess(Object result) {
+			// Do nothing
+		}
+
+		@Override
+		public void onFailure(Throwable t) {
+			Collection<Runnable> nonExecutedTasks = abort();
+			// Avoids multiple useless message due to the other tasks's cancellation when aborting
+			if (!nonExecutedTasks.isEmpty()) {
+				LOGGER.error("{} - Aborted execution of {} tasks because the following exception was caught when running a task",
+						loggerContext, nonExecutedTasks.size(), t);
+			}
+		}
+
+		private Collection<Runnable> abort() {
+			// Stop accepting tasks
+			executor.shutdown();
+			
+			// Remove all tasks that were not already executed
+			List<Runnable> nonExecutedTasks = new ArrayList<Runnable>();
+			executor.getQueue().drainTo(nonExecutedTasks);
+			
+			// Cancel these tasks (if relevant)
+			for (Runnable nonExecutedTask : nonExecutedTasks) {
+				if (nonExecutedTask instanceof Future<?>) {
+					Future<?> future = (Future<?>) nonExecutedTask;
+					future.cancel(false);
+				}
+			}
+			
+			return nonExecutedTasks;
+		}
+
 	}
 
 }
